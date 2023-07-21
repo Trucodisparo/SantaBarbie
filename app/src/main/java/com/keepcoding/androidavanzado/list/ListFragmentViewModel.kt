3827dccc11d466ec34dbd92f642fe1260d0ee52e
Daniel Calvo Sanz <daniel.calvo@gruposantander.com>
2023-07-21 12:42:03 +0200

    Live data implementado

--- app/src/main/java/com/keepcoding/androidavanzado/list/ListFragmentViewModel.kt
+++ app/src/main/java/com/keepcoding/androidavanzado/list/ListFragmentViewModel.kt
@@ -1,11 +1,19 @@
 package com.keepcoding.androidavanzado.list
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ListFragmentViewModel: ViewModel() {
-    private var list = listOf("Goku", "Vegeta", "Piccolo", "Frieza", "Krillin")
+    private var _heroList = MutableLiveData(listOf("Goku", "Vegeta", "Piccolo", "Frieza", "Krillin"))
+
+    val heroList: LiveData<List<String>> get() = _heroList
 
     fun getHeroList(): List<String>{
-        return list
+        return heroList.value ?: listOf()
     }
+
+    /*fun getHeroList(): List<String>{
+        return list
+    }*/
 }
